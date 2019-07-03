eec3e8a1f3a955dea8fa7edaa7acec219278f820
neo238291 <neonoir@outlook.jp>
2019-07-03 19:17:51 +0900

    Update main.js

--- main.js
+++ main.js
@@ -75,7 +75,12 @@ function text_match(){
         var video_id="surprise";
         var div_id="charaSurprise";
         play_movie(video_id,div_id);
+    }else  if(~rtnString.indexOf("秋葉原に行きたい")){
+        window.open('https://goo.gl/maps/pPWtGkfzTwsydfBUA', '_blank')
+	    voiceRec();
+	    
     }
+    
 }
 //ビデオ再生用関数
 function play_movie(video_id,div_id){
